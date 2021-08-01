a96087e060d3d06bf42b1195cdba66b717188efc
Kharis <hh_shamsuarov@bingo-boom.com>
2021-08-01 22:32:22 +0300

    init

--- go.mod
+++ go.mod
@@ -2,4 +2,29 @@ module ledger-pay
 
 go 1.16
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/TRON-US/go-btfs-config v0.10.0
+	github.com/btcsuite/btcd v0.22.0-beta // indirect
+	github.com/ethereum/go-ethereum v1.10.6 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.4.3
+	github.com/google/uuid v1.3.0
+	github.com/gorilla/mux v1.8.0
+	github.com/klauspost/cpuid/v2 v2.0.8 // indirect
+	github.com/libp2p/go-libp2p-core v0.9.0
+	github.com/multiformats/go-multiaddr v0.4.0 // indirect
+	github.com/multiformats/go-multihash v0.0.15 // indirect
+	github.com/tron-us/go-btfs-common v0.7.25
+	github.com/tron-us/protobuf v1.3.7
+	github.com/tyler-smith/go-bip32 v1.0.0
+	github.com/tyler-smith/go-bip39 v1.1.0
+	go.uber.org/multierr v1.6.0 // indirect
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+)
+
+replace github.com/ipfs/go-cid => github.com/TRON-US/go-cid v0.3.0
+
+replace github.com/libp2p/go-libp2p-core => github.com/TRON-US/go-libp2p-core v0.7.1
+
+replace github.com/multiformats/go-multiaddr => github.com/TRON-US/go-multiaddr v0.4.0
--- main.go
+++ main.go
@@ -1,54 +1,293 @@
 package main
 
 import (
+	"context"
+	"crypto/ecdsa"
+	"encoding/hex"
 	"fmt"
+	config "github.com/TRON-US/go-btfs-config"
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
+	ic "github.com/libp2p/go-libp2p-core/crypto"
+	"github.com/tron-us/go-btfs-common/crypto"
+	"github.com/tron-us/go-btfs-common/ledger"
+	escrowpb "github.com/tron-us/go-btfs-common/protos/escrow"
+	ledgerpb "github.com/tron-us/go-btfs-common/protos/ledger"
+	"github.com/tron-us/go-btfs-common/utils/grpc"
+	"github.com/tron-us/protobuf/proto"
 	"io"
+	"ledger-pay/util"
+	"ledger-pay/wallet"
 	"net/http"
 	"os"
+	"strconv"
 )
+
+var escrowService = "https://escrow.btfs.io"
+var taxLedger []byte
+var taxPercent int64 = 5
 
 // TODO 5%
 func main() {
+	taxLedger, _ = hex.DecodeString("")
+
 	r := mux.NewRouter()
 	r.HandleFunc("/transfer", transfer)
-	r.HandleFunc("/balance", balance)
+	//r.HandleFunc("/balance", balance)
+	r.HandleFunc("/", info)
 
 	http.Handle("/", r)
+	fmt.Println("Port: 30080")
 	err := http.ListenAndServe(":30080", nil)
 	if err != nil {
 		fmt.Printf("Error ListenAndServe: %s\n", err)
 		os.Exit(1)
 	}
-	fmt.Println("Port: 30080")
 }
 
 func transfer(w http.ResponseWriter, r *http.Request) {
+	var err error
+	requestId := uuid.New().String()
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	io.WriteString(w, "Request ID: "+requestId+"\n")
 	q := r.URL.Query()
-	if q.Get("toKey") != "" {
 
+	// FROM
+	var payerKey *ecdsa.PrivateKey
+	var payerLedger []byte
+	var payerIdentity config.Identity
+	if q.Get("fromKey") != "" {
+		var err error
+		payerKey, payerLedger, payerIdentity, err = getKeys(q.Get("fromKey"), "secp256k1", "")
+		if err != nil {
+			fmt.Println(requestId, "Error fromKey:", err)
+			io.WriteString(w, "Error fromKey: "+err.Error())
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
 	}
 
-	// Инициализируем оба кошелька
+	if q.Get("fromSeed") != "" {
+		var err error
+		payerKey, payerLedger, payerIdentity, err = getKeys("", "BIP39", q.Get("fromSeed"))
+		if err != nil {
+			fmt.Println(requestId, "Error fromSeed:", err)
+			io.WriteString(w, "Error fromSeed: "+err.Error())
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	}
 
-	// Делаем перевод
+	// TO (ledger)
+	var recipientLedger []byte
+	if q.Get("toKey") != "" {
+		var err error
+		_, recipientLedger, _, err = getKeys(q.Get("toKey"), "secp256k1", "")
+		if err != nil {
+			fmt.Println(requestId, "Error toKey:", err)
+			io.WriteString(w, "Error toKey: "+err.Error())
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	}
+
+	if q.Get("toSeed") != "" {
+		var err error
+		_, recipientLedger, _, err = getKeys("", "BIP39", q.Get("toSeed"))
+		if err != nil {
+			fmt.Println(requestId, "Error toSeed:", err)
+			io.WriteString(w, "Error toSeed: "+err.Error())
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	}
+
+	if q.Get("toSpeed") != "" {
+		var err error
+		recipientLedger, err = hex.DecodeString(q.Get("toSpeed"))
+		if err != nil {
+			fmt.Println(requestId, "Error toSpeed:", err)
+			io.WriteString(w, "Error toSpeed: "+err.Error())
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	}
+
+	payerBalanceBefore, err := getInAppBalance(&payerIdentity)
+	if err != nil {
+		fmt.Println(requestId, "Error getInAppBalance:", err)
+		io.WriteString(w, "Error getInAppBalance: "+err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	io.WriteString(w, "Balance before: "+strconv.FormatInt(payerBalanceBefore, 10)+"\n")
+
+	if q.Get("amount") != "" {
+		amount, err := strconv.ParseInt(q.Get("amount"), 10, 64)
+		if err != nil {
+			fmt.Println(requestId, "Error amount:", err)
+			io.WriteString(w, "Error amount: "+err.Error())
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if amount > payerBalanceBefore {
+			io.WriteString(w, "Low balance!")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		payerBalanceBefore = amount
+	}
+
+	taxSum := payerBalanceBefore * taxPercent / 100
+	if taxSum == 0 {
+		taxSum += 1
+	}
+	paySum := payerBalanceBefore - taxSum
+	if paySum == 0 {
+		io.WriteString(w, "Low amount, min: 2")
+		return
+	}
+
+	_, err = pay(payerKey, payerLedger, recipientLedger, paySum)
+	payInfo, err := pay(payerKey, payerLedger, taxLedger, taxSum)
+	if err != nil {
+		fmt.Println(requestId, "Error transfer:", err)
+		io.WriteString(w, "Error transfer: "+err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	io.WriteString(w, "Transfer sum: "+strconv.FormatInt(paySum, 10)+"\n")
+	io.WriteString(w, "Tax sum: "+strconv.FormatInt(taxSum, 10)+"\n")
+	io.WriteString(w, payInfo)
 
 	// Составляем ответ. Балансы до, балансы после, сумма перевода и комиссии, адрес кошелька трон и спида
 
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
-
-	fmt.Println(q)
 	w.WriteHeader(http.StatusOK)
+}
 
-	io.WriteString(w, "This HTTP response has both headers before this text and trailers at the end.\n")
-	io.WriteString(w, "This HTTP response has both headers before this text and trailers at the end.\n")
-	io.WriteString(w, "This HTTP response has both headers before this text and trailers at the end.\n")
+func getInAppBalance(identity *config.Identity) (int64, error) {
+	privKey, err := identity.DecodePrivateKey("")
+	if err != nil {
+		return 0, err
+	}
+	lgSignedPubKey, err := ledger.NewSignedPublicKey(privKey, privKey.GetPublic())
+
+	var balance int64 = 0
+	err = grpc.EscrowClient(escrowService).WithContext(context.Background(),
+		func(ctx context.Context, client escrowpb.EscrowServiceClient) error {
+			res, err := client.BalanceOf(ctx, ledger.NewSignedCreateAccountRequest(lgSignedPubKey.Key, lgSignedPubKey.Signature))
+			if err != nil {
+				return err
+			}
+			balance = res.Result.Balance
+			return nil
+		})
+	if err != nil {
+		return 0, err
+	}
+
+	return balance, nil
+}
+
+func pay(payerKey *ecdsa.PrivateKey, payerLedger []byte, recipientLedger []byte, amount int64) (payInfo string, err error) {
+	transferRequest := &ledgerpb.TransferRequest{
+		Payer:     &ledgerpb.PublicKey{Key: payerLedger},
+		Recipient: &ledgerpb.PublicKey{Key: recipientLedger},
+		Amount:    amount,
+	}
+
+	raw, err := proto.Marshal(transferRequest)
+	if err != nil {
+		return "", err
+	}
+
+	signature, err := wallet.SignChannel(raw, payerKey)
+	if err != nil {
+		return "", err
+	}
+
+	request := &ledgerpb.SignedTransferRequest{
+		TransferRequest: transferRequest,
+		Signature:       signature,
+	}
+
+	err = grpc.EscrowClient(escrowService).WithContext(context.Background(),
+		func(ctx context.Context, client escrowpb.EscrowServiceClient) error {
+			response, err := client.Pay(ctx, request)
+			if err != nil {
+				return err
+			}
+			if response == nil {
+				return fmt.Errorf("escrow reponse is nil")
+			}
+			payInfo = "Balance after:" + strconv.FormatInt(response.Balance, 10) + "\n"
+			return nil
+		})
+	if err != nil {
+		return "", err
+	}
+
+	return payInfo, nil
+}
+
+func getKeys(importKey string, keyType string, seedPhrase string) (key *ecdsa.PrivateKey, ledgerAddress []byte, identity config.Identity, err error) {
+	k, _, err := util.GenerateKey(importKey, keyType, seedPhrase)
+
+	ks, err := crypto.FromPrivateKey(k)
+	fmt.Println("Address", ks.Base58Address)
+	if err != nil {
+		return nil, nil, config.Identity{}, err
+	}
+	k64, err := crypto.Hex64ToBase64(ks.HexPrivateKey)
+	identity.PrivKey = k64
+	if err != nil {
+		return nil, nil, config.Identity{}, err
+	}
+
+	// get key
+	privKeyIC, err := identity.DecodePrivateKey("")
+	if err != nil {
+		return nil, nil, config.Identity{}, err
+	}
+	// base64 key
+	privKeyRaw, err := privKeyIC.Raw()
+	if err != nil {
+		return nil, nil, config.Identity{}, err
+	}
+	// hex key
+	hexPrivKey := hex.EncodeToString(privKeyRaw)
+
+	// hex key to ecdsa
+	privateKey, err := crypto.HexToECDSA(hexPrivKey)
+	if err != nil {
+		return nil, nil, config.Identity{}, err
+	}
+	if privateKey == nil {
+		fmt.Println("wallet get private key ecdsa failed")
+		return nil, nil, config.Identity{}, err
+	}
+
+	ledgerAddress, err = ic.RawFull(privKeyIC.GetPublic())
+
+	if err != nil {
+		fmt.Println("get ledger address failed, ERR: \n", err)
+		return nil, nil, config.Identity{}, err
+	}
+
+	return privateKey, ledgerAddress, identity, nil
 }
 
 func balance(w http.ResponseWriter, r *http.Request) {
-	//vars := mux.Vars(r)
-	//status := "AWAITING"
-	//id, _ := strconv.Atoi(vars["id"])
-
 	w.WriteHeader(http.StatusOK)
+}
+
+func info(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
+	w.WriteHeader(http.StatusOK)
+
+	io.WriteString(w, "\n")
+	io.WriteString(w, "This HTTP response has both headers before this text and trailers at the end.\n")
+	io.WriteString(w, "This HTTP response has both headers before this text and trailers at the end.\n")
 }
