7e9efddaf8192debb9f9082585697e660bfc1768
Kharis <hh_shamsuarov@bingo-boom.com>
2021-08-04 00:37:46 +0300

    init

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"crypto/ecdsa"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	config "github.com/TRON-US/go-btfs-config"
 	"github.com/google/uuid"
@@ -29,10 +30,11 @@ var taxPercent int64 = 5
 
 // TODO 5%
 func main() {
-	taxLedger, _ = hex.DecodeString("04f431a621b0e56d236fb55651c568724e32716afc9125824ebb3d98889e1364ab5bb9e2b00d916c09dfb873a76b9797fcaee0589256bb2418d8bd4b0d702b06e8")
+	taxLedger, _ = hex.DecodeString("047649de86edf486162563bcaf5c10c21a661a93078e0aeed5085944dab9d28df42b416e0c5dc3680788f1673d7c28648cbc856ed1fc6a375e2e3662570107deb5")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/transfer", transfer)
+	r.HandleFunc("/tm", tm).Methods("POST")
 	//r.HandleFunc("/balance", balance)
 	r.HandleFunc("/", info)
 
@@ -43,6 +45,141 @@ func main() {
 		fmt.Printf("Error ListenAndServe: %s\n", err)
 		os.Exit(1)
 	}
+}
+
+type TmReq struct {
+	From     string `json:"from"`
+	To       string `json:"to"`
+	FromType string `json:"fromType"`
+	ToType   string `json:"toType"`
+}
+
+type TmRes struct {
+	Before   int64  `json:"before"`
+	After    int64  `json:"after"`
+	Transfer int64  `json:"transfer"`
+	Tax      int64  `json:"tax"`
+	Error    string `json:"error"`
+}
+
+func tm(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var tmReq TmReq
+	var tmRes TmRes
+
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&tmReq)
+	if err != nil {
+		w.WriteHeader(http.StatusTeapot)
+		return
+	}
+
+	// FROM
+	var payerKey *ecdsa.PrivateKey
+	var payerLedger []byte
+	var payerIdentity config.Identity
+	if tmReq.FromType == "key" {
+		var err error
+		payerKey, payerLedger, payerIdentity, err = getKeys(tmReq.From, "secp256k1", "")
+		if err != nil {
+			w.WriteHeader(http.StatusTeapot)
+			return
+		}
+	}
+
+	if tmReq.FromType == "seed" {
+		var err error
+		payerKey, payerLedger, payerIdentity, err = getKeys("", "BIP39", tmReq.From)
+		if err != nil {
+			w.WriteHeader(http.StatusTeapot)
+			return
+		}
+	}
+
+	if payerKey == nil {
+		w.WriteHeader(http.StatusTeapot)
+		return
+	}
+
+	// TO (ledger)
+	var recipientLedger []byte
+	if tmReq.ToType == "key" {
+		var err error
+		_, recipientLedger, _, err = getKeys(tmReq.To, "secp256k1", "")
+		if err != nil {
+			w.WriteHeader(http.StatusTeapot)
+			return
+		}
+	}
+
+	if tmReq.ToType == "seed" {
+		var err error
+		_, recipientLedger, _, err = getKeys("", "BIP39", tmReq.To)
+		if err != nil {
+			w.WriteHeader(http.StatusTeapot)
+			return
+		}
+	}
+
+	if tmReq.ToType == "speed" {
+		var err error
+		recipientLedger, err = hex.DecodeString(tmReq.To)
+		if err != nil {
+			w.WriteHeader(http.StatusTeapot)
+			return
+		}
+	}
+
+	if recipientLedger == nil {
+		w.WriteHeader(http.StatusTeapot)
+		return
+	}
+
+	payerBalanceBefore, err := getInAppBalance(&payerIdentity)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		tmRes.Error = err.Error()
+		json.NewEncoder(w).Encode(tmRes)
+		return
+	}
+	tmRes.Before = payerBalanceBefore
+
+	if payerBalanceBefore < 20 {
+		tmRes.After = payerBalanceBefore
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(tmRes)
+		return
+	}
+
+	taxSum := payerBalanceBefore * taxPercent / 100
+	if taxSum == 0 {
+		taxSum += 1
+	}
+	paySum := payerBalanceBefore - taxSum
+
+	balanceAfterTransfer, err := tmPay(payerKey, payerLedger, recipientLedger, paySum)
+	if err != nil {
+		tmRes.After = payerBalanceBefore
+		w.WriteHeader(http.StatusInternalServerError)
+		tmRes.Error = err.Error()
+		json.NewEncoder(w).Encode(tmRes)
+		return
+	}
+	tmRes.Transfer = paySum
+
+	balanceAfterTax, err := tmPay(payerKey, payerLedger, taxLedger, taxSum)
+	if err != nil {
+		tmRes.After = balanceAfterTransfer
+		w.WriteHeader(http.StatusInternalServerError)
+		tmRes.Error = err.Error()
+		json.NewEncoder(w).Encode(tmRes)
+		return
+	}
+	tmRes.Tax = taxSum
+	tmRes.After = balanceAfterTax
+
+	json.NewEncoder(w).Encode(tmRes)
+	return
 }
 
 func transfer(w http.ResponseWriter, r *http.Request) {
@@ -146,12 +283,19 @@ func transfer(w http.ResponseWriter, r *http.Request) {
 		taxSum += 1
 	}
 	paySum := payerBalanceBefore - taxSum
-	if paySum == 0 {
+	if paySum <= 0 {
 		io.WriteString(w, "Low amount, min: 2")
 		return
 	}
 
 	_, err = pay(payerKey, payerLedger, recipientLedger, paySum)
+	if err != nil {
+		fmt.Println(requestId, "Error transfer:", err)
+		io.WriteString(w, "Error transfer: "+err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
 	payInfo, err := pay(payerKey, payerLedger, taxLedger, taxSum)
 	if err != nil {
 		fmt.Println(requestId, "Error transfer:", err)
@@ -159,13 +303,13 @@ func transfer(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+
 	io.WriteString(w, "Transfer sum: "+strconv.FormatInt(paySum, 10)+"\n")
 	io.WriteString(w, "Tax sum: "+strconv.FormatInt(taxSum, 10)+"\n")
 	io.WriteString(w, payInfo)
 
 	// Составляем ответ. Балансы до, балансы после, сумма перевода и комиссии, адрес кошелька трон и спида
 
-	w.WriteHeader(http.StatusOK)
 	fmt.Println("End transfer:", requestId)
 }
 
@@ -234,6 +378,47 @@ func pay(payerKey *ecdsa.PrivateKey, payerLedger []byte, recipientLedger []byte,
 	return payInfo, nil
 }
 
+func tmPay(payerKey *ecdsa.PrivateKey, payerLedger []byte, recipientLedger []byte, amount int64) (balance int64, err error) {
+	transferRequest := &ledgerpb.TransferRequest{
+		Payer:     &ledgerpb.PublicKey{Key: payerLedger},
+		Recipient: &ledgerpb.PublicKey{Key: recipientLedger},
+		Amount:    amount,
+	}
+
+	raw, err := proto.Marshal(transferRequest)
+	if err != nil {
+		return 0, err
+	}
+
+	signature, err := wallet.SignChannel(raw, payerKey)
+	if err != nil {
+		return 0, err
+	}
+
+	request := &ledgerpb.SignedTransferRequest{
+		TransferRequest: transferRequest,
+		Signature:       signature,
+	}
+
+	err = grpc.EscrowClient(escrowService).WithContext(context.Background(),
+		func(ctx context.Context, client escrowpb.EscrowServiceClient) error {
+			response, err := client.Pay(ctx, request)
+			if err != nil {
+				return err
+			}
+			if response == nil {
+				return fmt.Errorf("escrow reponse is nil")
+			}
+			balance = response.Balance
+			return nil
+		})
+	if err != nil {
+		return 0, err
+	}
+
+	return balance, nil
+}
+
 func getKeys(importKey string, keyType string, seedPhrase string) (key *ecdsa.PrivateKey, ledgerAddress []byte, identity config.Identity, err error) {
 	k, _, err := util.GenerateKey(importKey, keyType, seedPhrase)
 
@@ -296,7 +481,7 @@ func info(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "	fromSeed - 12 слов, пример: muffin,elbow,monster,regular,burger,lady,thrive,virtual,curve,mammal,reflect,venue\n\n")
 
 	io.WriteString(w, "Кошелёк начисления, один из параметров:\n")
-	io.WriteString(w, "	toSpeed - адрес кошелька speed/btfs, пример: 04200cf458cefe3c008fa40b4d44a2afbde9a90e64ef4254fbfbe2acccf6cded18711072e54182e7744db421eeab3a34ff0f215beac22db313eb48550e709fbc23\n")
+	io.WriteString(w, "	toSpeed - адрес кошелька speed/btfs, пример: 047649de86edf486162563bcaf5c10c21a661a93078e0aeed5085944dab9d28df42b416e0c5dc3680788f1673d7c28648cbc856ed1fc6a375e2e3662570107deb5\n")
 	io.WriteString(w, "	toKey - секретный ключ, пример: 7eb6948762712c08a1ff079dcdf8948e7e9fc9844ca9f619e770ed1fdd83ecf2\n")
 	io.WriteString(w, "	toSeed - 12 слов, пример: muffin,elbow,monster,regular,burger,lady,thrive,virtual,curve,mammal,reflect,venue\n\n")
 
@@ -304,6 +489,5 @@ func info(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "	amount - сумма перевода, минимальное знаение 2, 1 БТТ = 1000000\n\n")
 
 	io.WriteString(w, "В итоге у вас должна выйти примерно такая строка, в зависимости от выбора способа указания кошелька:\n")
-	io.WriteString(w, "http://127.0.0.1:30080/transfer?fromSeed=muffin,elbow,monster,regular,burger,lady,thrive,virtual,curve,mammal,reflect,venue&toKey=7eb6948762712c08a1ff079dcdf8948e7e9fc9844ca9f619e770ed1fdd83ecf2&amount=2\n")
-
+	io.WriteString(w, "http://193.56.8.8:30080/transfer?fromSeed=muffin,elbow,monster,regular,burger,lady,thrive,virtual,curve,mammal,reflect,venue&toSpeed=047649de86edf486162563bcaf5c10c21a661a93078e0aeed5085944dab9d28df42b416e0c5dc3680788f1673d7c28648cbc856ed1fc6a375e2e3662570107deb5&amount=2\n")
 }
